fe9f16866c5e96f973336b998b94bd10f19ecc32
Dtesch9 <alucard.s8123@gmail.com>
2020-02-17 21:40:01 -0300

    (Home) Product container

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container, List } from './styles';
+import {
+  Container,
+  Product,
+  ProductImage,
+  Description,
+  Price,
+  ButtonContainer,
+  CartAmount,
+  Amount,
+  AddButtonText,
+} from './styles';
+import api from '../../services/api';
 
-export default function Home() {
-  return <Container />;
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get('products');
+
+    this.setState({ products: response.data[0] });
+  }
+
+  render() {
+    const { products } = this.state;
+
+    return (
+      <Container>
+        <Product>
+          <ProductImage source={{ uri: products.image }} />
+          <Description>{products.title}</Description>
+          <Price>{products.price}</Price>
+          {/* <ButtonContainer onPress={() => {}}>
+            <CartAmount>
+              <Icon name="add-shopping-cart" color="#fff" size={20} />
+              <Amount>{0}</Amount>
+            </CartAmount>
+            <AddButtonText>adicionar</AddButtonText>
+          </ButtonContainer> */}
+        </Product>
+      </Container>
+    );
+  }
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -3,6 +3,55 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   background: #191920;
   flex: 1;
+  padding: 20px;
 `;
 
-export const List = styled.FlatList``;
+export const Product = styled.View`
+  margin-top: 10px;
+  background-color: white;
+  width: 230px;
+  height: 360px;
+  border-radius: 6px;
+  padding: 20px;
+  align-items: flex-start;
+`;
+
+export const ProductImage = styled.Image`
+  width: 200px;
+  height: 200px;
+`;
+
+export const Description = styled.Text`
+  margin-top: 10px;
+  font-size: 16px;
+  font-weight: normal;
+  line-height: 21px;
+  color: #333333;
+`;
+
+export const Price = styled.Text`
+  margin-top: 10px;
+  font-weight: bold;
+  font-size: 21px;
+  line-height: 25px;
+  color: #000000;
+`;
+
+// export const ButtonContainer = styled.TouchableOpacity`
+//   margin-top: auto;
+//   flex-direction: row;
+//   width: 200px;
+//   height: 42px;
+//   padding: 12px;
+// `;
+// export const CartAmount = styled.View`
+//   flex-direction: row;
+// `;
+// export const Amount = styled.text`
+//   color: '#fff';
+// `;
+// export const AddButtonText = styled.Text`
+//   flex: 1;
+//   text-transform: uppercase;
+//   font-weight: bold;
+// `;
